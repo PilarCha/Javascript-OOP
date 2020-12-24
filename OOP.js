a67077c6514551af2f3020aaefcc4bd5feecb32d
PilarCha <pchavez92@gmail.com>
2020-12-23 18:50:48 -0800

    created book 1

--- OOP.js
+++ OOP.js
@@ -1,1 +1,11 @@
-console.log('Hello OOP')
+//object literal
+const book1 = {
+  title: 'Book One',
+  author: 'John Doe',
+  year: '2013',
+  getSummary: function() {
+    return `${this.title} was written ${this.author} in the year of ${this.year};`
+  }
+}
+
+console.log(book1.title)
