906f168712ea4de98a94218b79f5f3bd728d0fbc
PilarCha <pchavez92@gmail.com>
2020-12-25 14:18:19 -0800

    Prototype on the 100

--- 2_constructor.js
+++ 2_constructor.js
@@ -3,6 +3,10 @@ function Book(title,author,year) {
   this.title = title;
   this.author = author;
   this.year = year;
+
+  this.getSummary = function () {
+    return `${this.title} was written by ${this.author} in the year of ${this.year};`
+  }
 }
 
 //instatiate an object
---
+++ 3_Prototype.js
@@ -0,0 +1,31 @@
+// Constructor
+function Book(title,author,year) {
+  this.title = title;
+  this.author = author;
+  this.year = year;
+}
+// prototype is available for all to use. yay.
+Book.prototype.getSummary = function() {
+  return `${this.title} was written by ${this.author} in the year of ${this.year};`
+}
+
+//getAge get years that passsed
+Book.prototype.getAge = function() {
+  const years = new Date().getFullYear() - this.year;
+  return `${this.title} is ${years} years old`
+}
+
+//manipulate data
+Book.prototype.revise = function (newYear) {
+  this.year = newYear;
+  this.revised = true; // revised will be added to the object literal
+}
+
+
+//instatiate an object
+const book1 = new Book('book1', 'assman', 2012);
+const book2 = new Book('book2', 'boobyman', 2016);
+
+console.log(book2);
+book2.revise(2020);
+console.log(book2)
