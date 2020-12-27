320fac99ab6e6484bfcf6af72e8f578084ba158a
PilarCha <pchavez92@gmail.com>
2020-12-26 16:35:21 -0800

    finishing up inheritence

--- 4_Inheritence.js
+++ 4_Inheritence.js
@@ -8,3 +8,25 @@ function Book(title,author,year) {
 Book.prototype.getSummary = function() {
   return `${this.title} was written by ${this.author} in the year of ${this.year};`
 }
+
+//mag constructor
+function Magazine(title,author,year,month) {
+  Book.call(this,title,author,year);
+
+  this.month = month;
+
+}
+
+function Textbook(title,author,year,edition,publisher) {
+  Book.call(this,title,author,year);
+
+  this.edition = edition;
+  this.publisher = publisher;
+}
+
+const mag1 = new Magazine('Big booty', 'john doe', 2012, 'Dec')
+const text1 = new Textbook('Words','Words boy', 2020, '5th', 'Penguin')
+
+console.log(mag1)
+
+console.log(text1)
