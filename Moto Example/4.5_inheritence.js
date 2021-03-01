f91d4baf3442db7829aa08b5ef80eb20ab88ed46
PilarCha <pchavez92@gmail.com>
2021-02-28 17:06:19 -0800

    finished doggo shindig

--- Moto Example/4.5_inheritence.js
+++ Moto Example/4.5_inheritence.js
@@ -12,17 +12,35 @@ class Dog {
 }
 
 class Labrador extends Dog {
-  constructor(name,weight) {
-    super(name,weight);
+  constructor(name) {
+    super(name);
+    this.weight = 75;
   }
   speak() {
     return 'WOOF';
   }
+  getWeight() {
+    return this.weight;
+  }
 
+}
+
+class Yorkshire extends Dog {
+  constructor(name) {
+    super(name);
+    this.weight = 10;
+  }
+  speak() {
+    return 'woof';
+  }
 }
 
 const doggo = new Dog('Bobbie',70);
 console.log(doggo.speak());
 
-const doggo1 = new Labrador('Homie',75);
+const doggo1 = new Labrador('Homie');
 console.log(doggo1.speak());
+console.log(doggo1.getWeight());
+
+const doggo2 = new Yorkshire('Shitty');
+console.log(doggo2.speak());
